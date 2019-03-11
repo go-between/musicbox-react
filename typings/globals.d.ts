f0395fc11cd5a08bd31a6ffbfd7aff4ca6eb2ee6
Truman Shuck <hello@trumanshuck.com>
2019-03-10 23:22:44 -0500

    [feat ] Use environment variables/constants instead of magic strings

--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -3,3 +3,12 @@ declare interface Window {
   __REDUX_DEVTOOLS_EXTENSION__: any
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any
 }
+
+declare var process: Process
+declare interface Process {
+  env: {
+    API_HOST: string
+    YOUTUBE_KEY: string
+    WS_HOST: string
+  }
+}
