5eadea77bc011833045469004b6de802146093e3
Truman Shuck <truman@hatchloyalty.com>
2019-03-31 02:06:15 -0500

    [feat ] Sorry for the big commit, adds graphql stuff for fetching users, creating songs, etc.

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,4 +1,5 @@
 import graphql from 'graphql.js'
+import { Options } from './types'
 
 export default class Client {
   songs: any = {
@@ -6,19 +7,51 @@ export default class Client {
       (@autodeclare) {
         createSong(input: { youtubeId: $youtubeId }) {
           song {
-            id
-            name
-            youtubeId
+            ...song
           }
           errors
         }
       }
-    `)({ youtubeId })
+    `)({ youtubeId }),
+
+    library: () => this.baseClient.query(`
+      {
+        songs {
+          ...song
+        }
+      }
+    `)
+  }
+
+  users: any = {
+    inRoom: (roomId) => this.baseClient.query(`
+      {
+        users {
+          ...user
+        }
+      }
+    `)({ roomId })
   }
 
   private baseClient: any
+  private fragments: any = {
+    song: 'on Song { id, description, durationInSeconds, name, youtubeId }',
+    user: 'on User { id, email } ',
+  }
 
-  constructor(host: string, authorizationCode: string) {
-    this.baseClient = graphql(host, { debug: true })
+  constructor(host: string, authorizationCode: string | null, options: Options) {
+    const graphQLOptions = {
+      debug: options.debug,
+      headers: {},
+      fragments: this.fragments,
+    }
+
+    if (authorizationCode !== null) {
+      graphQLOptions.headers = {
+        ...graphQLOptions.headers,
+        Authorization: `Bearer ${authorizationCode}`,
+      }
+    }
+    this.baseClient = graphql(host, graphQLOptions)
   }
 }
--- src/models/song/types.ts
+++ src/models/song/types.ts
@@ -1,16 +1,19 @@
-const CREATE_SONG = 'models/user/CREATE_SONG'
+const CREATE_SONG = 'models/song/CREATE_SONG'
+const GET_SONGS = 'models/song/GET_SONGS'
 
 type Types = {
   CREATE_SONG: typeof CREATE_SONG
+  GET_SONGS: typeof GET_SONGS
 }
 
 export const types: Types = {
-  CREATE_SONG
+  CREATE_SONG,
+  GET_SONGS,
 }
 
-type CreateSong = <OK, ERR>(song: Song, returnOK: OK, returnERR: ERR) => {
+type CreateSong = <OK, ERR>(youtubeId: Song['youtubeId'], returnOK: OK, returnERR: ERR) => {
   type: typeof CREATE_SONG,
-  song: Song,
+  youtubeId: Song['youtubeId'],
   returnOK: OK,
   returnERR: ERR,
 }
@@ -25,12 +28,35 @@ export type CreateSongERR<T> = (type: T, error: Error) => {
   error: Error,
 }
 
+type GetSongs = <OK, ERR>(returnOK: OK, returnERR: ERR) => {
+  type: typeof GET_SONGS,
+  returnOK: OK,
+  returnERR: ERR,
+}
+
+export type GetSongOK<T> = (type: T, songs: Song) => {
+  type: T,
+  song: Song,
+}
+
+export type GetSongsERR<T> = (type: T, error: Error) => {
+  type: T,
+  error: Error,
+}
+
 export type ActionCreators = {
   CreateSong: CreateSong
   CreateSongOK: CreateSongOK<any>
   CreateSongERR: CreateSongERR<any>
+  GetSongs: GetSongs,
+  GetSongOK: GetSongOK<any>
+  GetSongsERR: GetSongsERR<any>
 }
 
 export type Song = {
+  id: string
+  description: string
+  durationInSeconds: number
+  name: string
   youtubeId: string
 }
