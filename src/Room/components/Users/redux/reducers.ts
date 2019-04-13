4c9ae0fee2968a0149afaa2902caa6ee2c077cee
Truman Shuck <hello@trumanshuck.com>
2019-04-12 22:42:31 -0500

    [feat ] Allow auth to redirect

--- src/Room/components/Users/redux/reducers.ts
+++ src/Room/components/Users/redux/reducers.ts
@@ -1,5 +1,5 @@
 import { types as roomTypes } from 'Room/redux/types'
-import { Action, State } from './types'
+import { Action, State, types } from './types'
 
 export const initialState: State = {
   users: [],
@@ -8,7 +8,9 @@ export const initialState: State = {
 export default function reducer(state: State = initialState, action: Action): State {
   switch (action.type) {
     case roomTypes.JOIN_ROOM_OK:
-      return { ...state, users: action.room.users }
+      return { ...state, users: [...action.room.users] }
+    case types.UPDATE_USERS:
+      return { ...state, users: [...action.users] }
     default:
       return state
   }
