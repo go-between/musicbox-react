fd750125a3d3c73a3dfbb33bb46e7a5b6aad816d
Truman Shuck <hello@trumanshuck.com>
2019-07-01 23:20:48 -0500

    [feat ] Add some better guards around establishing websocket connection before rendering app

--- src/cable/index.ts
+++ src/cable/index.ts
@@ -1,2 +1,4 @@
+export { default as actions } from './actions'
 export * from './client'
+export { default as reducer } from './reducers'
 export * from './types'
--- src/cable/sagas.ts
+++ src/cable/sagas.ts
@@ -1,34 +1,37 @@
-import { take, call, put, select } from 'redux-saga/effects'
+import { take, call, put, select, takeLatest } from 'redux-saga/effects'
 import { eventChannel } from 'redux-saga'
 import { getSingleton } from './client'
 import { WS_HOST } from '../lib/constants'
 import { State } from 'reducers'
+import actions from './actions'
+import { types } from './types'
 
-function* initWebsocket(token: string) {
-  if (!token) {
-    return
-  }
+function* initWebsocket() {
+  const token = (yield select((s: State) => s.auth.token)) as string
 
   const formData = new URLSearchParams()
   formData.append('token', token)
 
   const ws = new WebSocket(`${WS_HOST}?${formData.toString()}`)
   const client = getSingleton()
-  return eventChannel(emitter => {
-    ws.onopen = () => client.bind(ws, emitter)
-    return () => console.log('Sign Off')
+  const channel = yield new Promise((resolve, reject) => {
+    const evChannel = eventChannel(emitter => {
+      ws.onopen = () => {
+        client.bind(ws, emitter)
+        resolve(evChannel)
+      }
+      return () => console.log('Sign Off')
+    })
   })
-}
 
-export default function* saga() {
-  let token = ''
-  while (!token) {
-    token = (yield select((s: State) => s.auth.token)) as string
-  }
+  yield put(actions.connectOK())
 
-  const channel = yield call(initWebsocket, token)
   while (true) {
     const action = yield take(channel)
     yield put(action)
   }
+}
+
+export default function* saga() {
+  yield takeLatest(types.CONNECT, initWebsocket)
 }
